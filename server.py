250b8396eb6d8223320f88c33c235e379da15c33
Dannykeren <dannykeren@gmail.com>
2025-04-14 16:45:44 +0300

    Update server.py

--- server.py
+++ server.py
@@ -16,13 +16,14 @@ import time
 
 # Set up logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
         logging.FileHandler("server.log"),
         logging.StreamHandler()
     ]
 )
+
 logger = logging.getLogger("server")
 
 # Create Flask app
